265aed066c7a58617e0b07a3aeec912f3844f15b
Sumrak1337 <sumrakpolyakov@mail.ru>
2022-04-20 00:30:50 +0300

    added clear data for every gender and for every district

--- src/data_processing/data_processing.py
+++ src/data_processing/data_processing.py
@@ -54,14 +54,39 @@ def men_women_processing(file_name, index):
     clear_df.to_excel(Path(clear_data_path, file_name), encoding='utf-8')
 
 
-def men_women_aged_processing(file_name, index):
-    order = np.array([1, 2, 3, 4, 15, 16, 17, 18, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0])
+def men_women_aged_processing(file_name, path, index, names):
+    order = np.array([1, 2, 3, 4, 15, 16, 17, 18, 19, 20, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0])
     column_names = np.array([year for year in range(2015, 2023)])
+    districts_number = 13
+    age_number = 21
+
+    tmp_df = pd.DataFrame()
+    main_df = pd.read_excel(Path(data_path, file_name))
+
+    for i, r in main_df.iterrows():
+        if i < 2:
+            continue
+        if (i - 2) % (age_number + 1) != 0:
+            tmp_df = tmp_df.append(r, ignore_index=True).astype(int, errors='ignore')
+
+    del tmp_df['Unnamed: 0']
+    del tmp_df['Unnamed: 1']
+    for i in range(districts_number):
+        tmp_clear_df = pd.DataFrame()
+        for j in range(age_number):
+            tmp_clear_df = tmp_clear_df.append(tmp_df.loc[j + age_number * i])
+
+        tmp_clear_df = tmp_clear_df.iloc[order]
+        tmp_clear_df.index = index
+        tmp_clear_df.columns = column_names
+        tmp_clear_df.to_excel(Path(path, file_name[:-5] + f'_{i}' + file_name[-5:]), encoding='utf-8')
 
 
 rep_path = pathlib.Path.cwd().resolve().parents[1]
 data_path = Path(rep_path, 'data')
 clear_data_path = Path(rep_path, 'clear_data')
+men_path = Path(clear_data_path, 'men')
+women_path = Path(clear_data_path, 'women')
 
 row_names = np.array(['Российская Федерация',
                       'Центральный ФО',
@@ -77,8 +102,31 @@ row_names = np.array(['Российская Федерация',
                       'Дальневосточный ФО (с 2019)',
                       'Крымский ФО'])
 
+# TODO: perhaps this can be simplified
+age_names = np.array(['0 - 4 года',
+                      '5 - 9 лет',
+                      '10 - 14 лет',
+                      '15 - 19 лет',
+                      '20 - 24 лет',
+                      '25 - 29 лет',
+                      '30 - 34 лет',
+                      '35 - 39 лет',
+                      '40 - 44 лет',
+                      '45 - 49 лет',
+                      '50 - 54 лет',
+                      '55 - 59 лет',
+                      '60 - 64 лет',
+                      '65 - 69 лет',
+                      '70 - 74 лет',
+                      '75 - 79 лет',
+                      '80 - 84 лет',
+                      '85 - 89 лет',
+                      '90 - 94 лет',
+                      '95 - 99 лет',
+                      '100+ лет'])
+
 population_processing('population_2015_2021.xlsx', row_names)
 men_women_processing('men_2015_2022.xlsx', row_names)
 men_women_processing('women_2015_2022.xlsx', row_names)
-men_women_aged_processing('men_2015_2022.xlsx', row_names)
-men_women_aged_processing('women_2015_2022.xlsx', row_names)
+men_women_aged_processing('men_2015_2022.xlsx', men_path, age_names, row_names)
+men_women_aged_processing('women_2015_2022.xlsx', women_path, age_names, row_names)

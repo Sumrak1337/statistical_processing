a0d6df748e6e77200d4f44cb7a947736352385e3
Sumrak1337 <sumrakpolyakov@mail.ru>
2022-04-26 15:32:27 +0300

    added general aged bar plots of men and women for each region

--- src/men_women_aged_descriptive_statistics.py
+++ src/men_women_aged_descriptive_statistics.py
@@ -8,6 +8,9 @@ from pathlib import Path
 rep_path = pathlib.Path.cwd().resolve().parents[0]
 results_path = Path(rep_path, 'results')
 clear_data_path = Path(rep_path, 'clear_data')
+men_data_path = Path(clear_data_path, 'men')
+women_data_path = Path(clear_data_path, 'women')
+generals_bars_path = Path(results_path, 'general_bars')
 men_results = Path(results_path, 'men')
 women_results = Path(results_path, 'women')
 
@@ -27,6 +30,7 @@ districts_names = np.array(['Российская Федерация',
 
 districts_number = len(districts_names)
 
+# for men/women plots
 for gender in ['men', 'women']:
     for ind in range(districts_number):
         df = pd.read_excel(Path(clear_data_path, f'{gender}', f'{gender}_2015_2022_{ind}.xlsx'))
@@ -46,3 +50,29 @@ for gender in ['men', 'women']:
             plt.grid()
             plt.savefig(Path(str(results_path) + f'/{gender}', f'{gender}_{ind}_{column}.png'))
             plt.close('all')
+
+# for general plots
+# for gender in ['men', 'women']:
+for i in range(districts_number):
+    df_men = pd.read_excel(Path(men_data_path, f'men_2015_2022_{i}.xlsx'))
+    df_women = pd.read_excel(Path(women_data_path, f'women_2015_2022_{i}.xlsx'))
+    index_name = df_men.iloc[:, 0].to_numpy()
+    del df_men["Unnamed: 0"]
+    del df_women["Unnamed: 0"]
+
+    for year in df_men.columns:
+        if np.all(np.isnan(df_men[year])):
+            continue
+
+        plt.figure(figsize=(15, 10))
+        plt.title(f'{districts_names[i]}, {year}')
+        x_lbl = np.arange(len(index_name))
+        plt.bar(x_lbl - 0.2, df_men[year] / 1e6, 0.4, label='men')
+        plt.bar(x_lbl + 0.2, df_women[year] / 1e6, 0.4, label='women')
+        plt.xticks(x_lbl, index_name, rotation=45)
+        plt.xlabel("Age")
+        plt.ylabel("Million people")
+        plt.legend()
+        plt.grid()
+        plt.savefig(Path(generals_bars_path, f'general_{i}_{year}.png'))
+        plt.close('all')

3d8a76f47a6b4e71b3a6a026c8aba546a9c95877
Sumrak1337 <sumrakpolyakov@mail.ru>
2022-05-20 01:19:39 +0300

    added regressions for men/women/general; some fixes

--- src/men_women_lin_reg.py
+++ src/men_women_lin_reg.py
@@ -4,7 +4,7 @@ import scipy.stats as stat
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 from pathlib import Path
 
 rep_path = Path.cwd().resolve().parents[0]
@@ -113,20 +113,28 @@ def corr_coeff():
 def lin_reg_for_mean():
     df_men = pd.read_excel(Path(clear_data_path, 'men_rf_2015_2022.xlsx'))
     df_women = pd.read_excel(Path(clear_data_path, 'women_rf_2015_2022.xlsx'))
+    df = df_men + df_women
     ages = df_men.iloc[:, 0]
     del df_men["Unnamed: 0"]
     del df_women["Unnamed: 0"]
+    del df["Unnamed: 0"]
     years = np.array(df_men.columns).reshape(-1, 1)
 
     men_mean_age = np.array([])
     women_mean_age = np.array([])
+    general_mean_age = np.array([])
 
     for year in df_men.columns:
         men_mean_age = np.append(men_mean_age, (df_men[year] @ ages + df_men[year][0]) / df_men[year].sum())
         women_mean_age = np.append(women_mean_age, (df_women[year] @ ages + df_women[year][0]) / df_women[year].sum())
+        general_mean_age = np.append(general_mean_age, (df[year] @ ages + df[year][0]) / df[year].sum())
 
-    reg_men = LinearRegression().fit(years, men_mean_age)
-    reg_women = LinearRegression().fit(years, women_mean_age)
+    reg_men = LinearRegression()
+    reg_men.fit(years, men_mean_age)
+    reg_women = LinearRegression()
+    reg_women.fit(years, women_mean_age)
+    reg_gen = LinearRegression()
+    reg_gen.fit(years, general_mean_age)
 
     plt.figure()
     plt.xlabel('years')
@@ -134,20 +142,28 @@ def lin_reg_for_mean():
     plt.title('Linear Regression for mean age')
     plt.scatter(years, men_mean_age, color=colors[3])
     plt.scatter(years, women_mean_age, color=colors[3])
+    plt.scatter(years, general_mean_age, color=colors[3])
     y_pred_men = reg_men.predict(years)
     y_pred_women = reg_women.predict(years)
+    y_pred_general = reg_gen.predict(years)
     r2_men = reg_men.score(years, men_mean_age)
     r2_women = reg_women.score(years, women_mean_age)
+    r2_general = reg_gen.score(years, general_mean_age)
     plt.plot(years, y_pred_men, linestyle='--', label=f'men\n'
-                                                      f'mce={mean_squared_error(men_mean_age, y_pred_men):.3}\n'
+                                                      f'mse={mean_squared_error(men_mean_age, y_pred_men):.3}\n'
                                                       f'coef={reg_men.coef_[0]:.3}\n'
                                                       f'$R^2$={r2_men:.6}')
     plt.plot(years, y_pred_women, linestyle='--', label=f'women\n'
-                                                        f'mce={mean_squared_error(women_mean_age, y_pred_women):.3}\n'
+                                                        f'mse={mean_squared_error(women_mean_age, y_pred_women):.3}\n'
                                                         f'coef={reg_women.coef_[0]:.3}\n'
                                                         f'$R^2$={r2_women:.6}')
+    plt.plot(years, y_pred_general, linestyle='--', label=f'general\n'
+                                                        f'mse={mean_squared_error(general_mean_age, y_pred_general):.3}\n'
+                                                        f'coef={reg_gen.coef_[0]:.3}\n'
+                                                        f'$R^2$={r2_general:.6}')
     plt.legend()
     plt.savefig(Path(str(results_path) + '/rf', 'linear_regression_for_mean_age.png'))
+    plt.close('all')
 
 
 def lin_reg_75():
@@ -159,8 +175,10 @@ def lin_reg_75():
     del df_women["Unnamed: 0"]
     years = np.array(df_men.index).reshape(-1, 1)
 
-    reg_men = LinearRegression().fit(years, df_men)
-    reg_women = LinearRegression().fit(years, df_women)
+    reg_men = LinearRegression()
+    reg_men.fit(years, df_men)
+    reg_women = LinearRegression()
+    reg_women.fit(years, df_women)
 
     plt.figure()
     plt.xlabel('years')
@@ -173,21 +191,162 @@ def lin_reg_75():
     r2_men = reg_men.score(years, df_men)
     r2_women = reg_women.score(years, df_women)
     plt.plot(years, y_pred_men, linestyle='--',
-             label=f'men,\n'
-                   f'mce={mean_squared_error(df_men, y_pred_men):.3}\n'
+             label=f'men\n'
+                   f'mse={mean_squared_error(df_men, y_pred_men):.3}\n'
                    f'coef={reg_men.coef_[0]:.3}\n'
                    f'$R^2=${r2_men:.6}')
     plt.plot(years, y_pred_women, linestyle='--',
-             label=f'women,\n'
-                   f'mce={mean_squared_error(df_women, y_pred_women):.3}\n'
+             label=f'women\n'
+                   f'mse={mean_squared_error(df_women, y_pred_women):.3}\n'
                    f'coef={reg_women.coef_[0]:.3}\n'
                    f'$R^2=${r2_women:.6}')
     plt.legend()
     plt.savefig(Path(str(results_path) + '/rf', 'linear_regression_for_75+.png'))
+    plt.close('all')
 
+
+def general_lin_reg():
+    df = pd.read_excel(Path(clear_data_path, 'population_2015_2021.xlsx'))
+    del df["Unnamed: 0"]
+    indices = df.columns
+    df_rf = np.array(df.iloc[0] / 1e6)
+    years = np.array(indices).astype(float)
+
+    # polynomial regression
+    fit2 = np.polyfit(years, df_rf, 2)
+    quad_reg = np.poly1d(fit2)
+    fit3 = np.polyfit(years, df_rf, 4)
+    quat_reg = np.poly1d(fit3)
+
+    # linear regression
+    years = years.reshape(-1, 1)
+    lin_reg = LinearRegression()
+    lin_reg.fit(years, df_rf)
+
+    # plotting
+    plt.figure()
+    plt.title('Linear and Polynomial Regression of Population RF')
+    plt.xlabel('years')
+    plt.ylabel('million people')
+    plt.scatter(years, df_rf, color=colors[3])
+    y_pred = lin_reg.predict(years)
+    plt.plot(years, y_pred, linestyle='--',
+             label=f'Linear Regression\n'
+                   f'coef={lin_reg.coef_[0]:.3}\n'
+                   f'mse={mean_squared_error(df_rf, y_pred):.3}\n'
+                   f'$R^2$={lin_reg.score(years, df_rf):.6}')
+    x = np.linspace(np.min(years), np.max(years))
+    y2 = quad_reg(x)
+    plt.plot(x, y2, linestyle='--',
+             label=f'Polynomial Regression(deg=2)\n'
+                   f'mse={mean_squared_error(df_rf, quad_reg(years)):.3}\n'
+                   f'$R^2$={r2_score(df_rf, quad_reg(years)):.6}')
+    y4 = quat_reg(x)
+    plt.plot(x, y4, linestyle='--',
+             label=f'Polynomial Regression(deg=4)\n'
+                   f'mse={mean_squared_error(df_rf, quat_reg(years)):.3}\n'
+                   f'$R^2$={r2_score(df_rf, quat_reg(years)):.6}')
+    plt.legend()
+    plt.savefig(Path(str(results_path) + '/rf', 'general_lin_reg.png'))
+    plt.close('all')
+
+
+def men_lin_reg():
+    df = pd.read_excel(Path(clear_data_path, 'men_rf_2015_2022.xlsx'))
+    df = df.sum() / 1e6
+    del df["Unnamed: 0"]
+    years = np.array(df.index).astype(float)
+    df = np.array(df)
+
+    # polynomial regression
+    fit2 = np.polyfit(years, df, 2)
+    quad_reg = np.poly1d(fit2)
+    fit3 = np.polyfit(years, df, 4)
+    quat_reg = np.poly1d(fit3)
+
+    # linear regression
+    years = years.reshape(-1, 1)
+    lin_reg = LinearRegression()
+    lin_reg.fit(years, df)
+
+    # plotting
+    plt.figure()
+    plt.title('Linear and Polynomial Regression of Men RF')
+    plt.xlabel('years')
+    plt.ylabel('million people')
+    plt.scatter(years, df, color=colors[3])
+    y_pred = lin_reg.predict(years)
+    plt.plot(years, y_pred, linestyle='--',
+             label=f'Linear Regression\n'
+                   f'coef={lin_reg.coef_[0]:.3}\n'
+                   f'mse={mean_squared_error(df, y_pred):.3}\n'
+                   f'$R^2$={lin_reg.score(years, df):.6}')
+    x = np.linspace(np.min(years), np.max(years))
+    y2 = quad_reg(x)
+    plt.plot(x, y2, linestyle='--',
+             label=f'Polynomial Regression(deg=2)\n'
+                   f'mse={mean_squared_error(df, quad_reg(years)):.3}\n'
+                   f'$R^2$={r2_score(df, quad_reg(years)):.6}')
+    y4 = quat_reg(x)
+    plt.plot(x, y4, linestyle='--',
+             label=f'Polynomial Regression(deg=4)\n'
+                   f'mse={mean_squared_error(df, quat_reg(years)):.3}\n'
+                   f'$R^2$={r2_score(df, quat_reg(years)):.6}')
+    plt.legend()
+    plt.savefig(Path(str(results_path) + '/rf', 'men_lin_reg.png'))
+    plt.close('all')
+
+
+def women_lin_reg():
+    df = pd.read_excel(Path(clear_data_path, 'women_rf_2015_2022.xlsx'))
+    df = df.sum() / 1e6
+    del df["Unnamed: 0"]
+    years = np.array(df.index).astype(float)
+    df = np.array(df)
+
+    # polynomial regression
+    fit2 = np.polyfit(years, df, 2)
+    quad_reg = np.poly1d(fit2)
+    fit3 = np.polyfit(years, df, 4)
+    quat_reg = np.poly1d(fit3)
+
+    # linear regression
+    years = years.reshape(-1, 1)
+    lin_reg = LinearRegression()
+    lin_reg.fit(years, df)
+
+    # plotting
+    plt.figure()
+    plt.title('Linear and Polynomial Regression of Women RF')
+    plt.xlabel('years')
+    plt.ylabel('million people')
+    plt.scatter(years, df, color=colors[3])
+    y_pred = lin_reg.predict(years)
+    plt.plot(years, y_pred, linestyle='--',
+             label=f'Linear Regression\n'
+                   f'coef={lin_reg.coef_[0]:.3}\n'
+                   f'mse={mean_squared_error(df, y_pred):.3}\n'
+                   f'$R^2$={lin_reg.score(years, df):.6}')
+    x = np.linspace(np.min(years), np.max(years))
+    y2 = quad_reg(x)
+    plt.plot(x, y2, linestyle='--',
+             label=f'Polynomial Regression(deg=2)\n'
+                   f'mse={mean_squared_error(df, quad_reg(years)):.3}\n'
+                   f'$R^2$={r2_score(df, quad_reg(years)):.6}')
+    y4 = quat_reg(x)
+    plt.plot(x, y4, linestyle='--',
+             label=f'Polynomial Regression(deg=4)\n'
+                   f'mse={mean_squared_error(df, quat_reg(years)):.3}\n'
+                   f'$R^2$={r2_score(df, quat_reg(years)):.6}')
+    plt.legend()
+    plt.savefig(Path(str(results_path) + '/rf', 'women_lin_reg.png'))
+    plt.close('all')
 
 # descriptive_stats()
 # general_plots()
 # corr_coeff()
 # lin_reg_for_mean()
 # lin_reg_75()
+# general_lin_reg()
+# men_lin_reg()
+# women_lin_reg()
